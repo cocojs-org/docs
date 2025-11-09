2437cdd6705c8c88b18836a696396d857e08b111
jcg <jiang.chenguang@outlook.com>
2025-11-09 21:31:36 +0800

    doc: 编写文档

--- src/page/learn/learn-advance-deep-in-component-page.tsx
+++ src/page/learn/learn-advance-deep-in-component-page.tsx
@@ -1,56 +1,113 @@
 import { route, page, reactive, bind, Router, autowired } from 'coco-mvc';
 import SideMenu from '@/view/side-menu';
-import { Header1, Header2, Code, InlineCode, Card, Button, Table } from 'coco-official-website-kit';
+import { Header1, Header2, Header3, InlineCode, Card, Code, Table } from 'coco-official-website-kit';
 import ContentLayout from '@/layout/content-layout';
 
 @route('/learn/advance-deep-in-component')
 @page()
 class LearnAdvanceDeepInComponentPage {
+    propertiesCode: string = `
+{
+    "bootComponents": {
+        "Router": {}
+    }
+}
+`
+    lifeCycleCode: string = `
+@api()
+class LoginApi {
+    init() {}
+    start() {}
+}
+`
+
     render() {
         return (
             <ContentLayout sideMenu={<SideMenu />}>
                 <Header1>深入组件</Header1>
-                <Header2>什么是组件？</Header2>
                 <div>
-                    在前端领域，组件一般是指视图组件，即一套包含了界面、状态、事件处理的可复用的逻辑单元。
-                    在coco-mvc中，组件的概念更广，上面介绍了视图组件、流程组件、工具组件、路由组件等等都是组件，不限于视图层。
-                </div>
-                <Card>
-                    从视图、行为、数据获取、工具库全部使用类表达组件有 2 个好处：
+                    <div>
+                        在前端领域，组件一般是指视图组件，即封装界面、状态、事件处理的可复用的逻辑单元。
+                        在coco-mvc中，组件不仅包含视图组件，还有流程组件、工具组件、路由组件等。
+                    </div>
+                    所有组件使用类表达组件有 2 个好处：
                     <ul>
                         <li>统一的代码风格</li>
                         <li>由框架统一实例化和依赖注入</li>
                     </ul>
-                </Card>
-                <Header2>组件装饰器</Header2>
-                组件装饰器是指添加了这些装饰器的类会被框架识别为组件的装饰器，下面列举了框架提供的组件装饰器：
+                </div>
+                <Header2>收集组件</Header2>
+                根据组件源文件所在的位置，可以分为 3 种：
                 <ul>
-                    <li>@component</li>
-                    <li>@view</li>
-                    <li>@page</li>
-                    <li>@layout</li>
-                    <li>@flow</li>
-                    <li>@globalData</li>
-                    <li>@render</li>
-                    <li>@api</li>
-                    <li>@util</li>
-                    <li>@localStorage</li>
-                    <li>@sessionStorage</li>
-                    <li>@cookie</li>
-                    <li>@store</li>
-                    <li>@router</li>
+                    <li>框架内部组件</li>
+                    <li>项目组件</li>
+                    <li>第三方组件</li>
                 </ul>
-                <Header2>框架提供的组件</Header2>
-                下面列举了框架提供的组件：
-                <ul>
-                    <li>Router</li>
-                    <li>Route</li>
-                    <li>LocalStorage</li>
-                    <li>SessionStorage</li>
-                    <li>Cookie</li>
-                </ul>
-                <Header1>自定义组件装饰器</Header1>
-                关于自定义组件装饰器，可以在创建装饰器一节中找到。
+                <div>
+                    框架内部组件是coco-mvc提供的常用的组件，包括：
+                    <ul>
+                        <li><InlineCode>Router</InlineCode>：用于页面跳转</li>
+                        <li><InlineCode>Route</InlineCode>：获取当前路由参数</li>
+                        <li><InlineCode>LocalStorage</InlineCode>：和浏览器的localStorage接口一样，为了统一使用</li>
+                        <li><InlineCode>SessionStorage</InlineCode>：和浏览器的sessionStorage接口一样，为了统一使用</li>
+                        <li><InlineCode>Cookie</InlineCode>：和浏览器的Cookie接口一样，为了统一使用</li>
+                    </ul>
+                </div>
+                <div>
+                    项目组件是指项目中添加了组件装饰器的类，这些组件都放在约定的文件夹中，框架在启动的时候自动收集。
+                </div>
+                <div>
+                    第三方组件是指npm包中导出的组件，具体见库开发。
+                </div>
+                <Header2>组件id</Header2>
+                组件id是组件的唯一标记，一般来说是类名，可通过<InlineCode>@id</InlineCode>装饰器进行指定。
+                <span className={'text-primary'}>框架收集到的所有组件的id都是唯一的</span>，在配置启动组件时可能需要用到组件id。
+                <Header2>实例化组件</Header2>
+                <Header3>组件的声明周期</Header3>
+                所有组件都有以下生命周期函数：
+                <Code code={this.lifeCycleCode} />
+                <Header3>单个组件的实例化过程</Header3>
+                <div>
+                    假设实例化的组件叫组件A
+                    <ul>
+                        <li>如果组件A 有<InlineCode>@constructorParam</InlineCode>装饰器，则每个依赖都执行单个组件的实例化过程，形成集合B</li>
+                        <li>使用集合B作为构造函数参数，实例化组件A</li>
+                        <li>如果组件A 存在<InlineCode>@autowired</InlineCode>字段，则每个依赖都执行单个组件的实例化过程，并赋值给字段</li>
+                        <li>
+                            因为可能有注入其他组件，所以最终可能会实例化多个组件，形成集合C
+                            <ul>
+                                <li>遍历集合C 中所有组件，如果有<InlineCode>init</InlineCode>方法的，执行<InlineCode>init</InlineCode>方法</li>
+                                <li>遍历集合C 中所有组件，如果有<InlineCode>start</InlineCode>方法的，执行<InlineCode>start</InlineCode>方法</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    所有类型的组件实例化过程都是一样的，视图组件在调和过程中会单独执行其他的生命周期函数，具体见视图组件。
+                </div>
+                <div>
+                    应用启动的时候，需要在动态配置中使用<InlineCode>bootComponents</InlineCode>指定应用启动时一起启动的组件，例如配置路由组件：
+                    <Code code={this.propertiesCode} />
+                    <InlineCode>bootComponents</InlineCode>对象的每个<InlineCode>key</InlineCode>都是组件id。
+                    如果<InlineCode>bootComponents</InlineCode>配置多个<InlineCode>key</InlineCode>，那么按序执行单个组件的实例化过程。
+                </div>
+                <div>
+                    所有需要实例化场景：
+                    <ul>
+                        <li>（项目中或第三方）在JSX中的视图组件。</li>
+                        <li>（项目中）被实例化的组件有<InlineCode>@autowired</InlineCode>装饰器的字段，实例化A的同时也要实例化组件B，且还要看组件B 是否包含<InlineCode>@autowired</InlineCode>字段</li>
+                        <li>（框架内部或项目中）任何组件A有<InlineCode>@constructParam</InlineCode>装饰器且构造函数入参是组件B，那么实例化A的同时也要实例化组件B</li>
+                        <li>（框架内部或项目中）调用<InlineCode>getComponent</InlineCode>接口，入参是想要被实例化的组件A</li>
+                        <li>（项目中）动态配置中，通过<InlineCode>bootComponents</InlineCode>字段配置的启动组件</li>
+                    </ul>
+                    <div>
+                        上面实例化场景中，如果要实例化的类没有子类，则可以直接实例化，如果有子类，则分情况讨论：
+                        <ul>
+                            <li>视图组件不会实例化子组件</li>
+                            <li>如果子组件只有一个，直接实例化子类</li>
+                            <li>如果子组件存在多个，需要使用<InlineCode>@qualify</InlineCode>装饰器或动态配置中<InlineCode>qualify</InlineCode>指定具体的子类id</li>
+                        </ul>
+                    </div>
+                </div>
+
             </ContentLayout>
         );
     }
--- src/page/learn/learn-advance-deep-in-decorator-page.tsx
+++ src/page/learn/learn-advance-deep-in-decorator-page.tsx
@@ -7,9 +7,10 @@ import ContentLayout from '@/layout/content-layout';
 @page()
 class LearnAdvanceDeepInDecoratorPage {
     viewDecorator: string = `
-import { Metadata } from 'coco-mvc';
+import { Metadata, component } from 'coco-mvc';
 
 @target([Target.Type.Class])
+@component()
 class View extends Metadata {}
 
 const view = createDecoratorExp(
@@ -31,39 +32,124 @@ class Button {
 export default View;
     `;
 
+    pageDecorator: string = `
+import { Metadata } from 'coco-ioc-container';
+import view from '../view';
+
+@view()
+class Page extends Metadata {}
+
+const page = createDecoratorExp(Page) as () => Decorator<ClassDecoratorContext>;
+
+export { Page, page };
+    `;
+
+    componentTreeCode: string = `
+component
+    |
+    +---view
+    |    |
+    |    +---page
+    |    +---layout
+    |
+    +---util
+    |    |
+    |    +---api
+    |    +---localStorage
+    |    +---sessionStorage
+    |    +---cookie
+    |
+    +---flow
+    |
+    +---store
+    |
+    +---globalData
+`
+
+
     render() {
         return (
             <ContentLayout sideMenu={<SideMenu />}>
                 <Header1>深入装饰器</Header1>
                 <div>
-                    装饰器是一种强大的编程特性，可以在不修改原有代码的情况下，给类及其成员添加额外的行为。ES
-                    的装饰器提案处于stage3阶段，具体语法见https://github.com/tc39/proposal-decorators。
-                </div>
-                <div>
-                    虽然规范规定了装饰器语法，但没规定运行时如何读取和使用装饰器参数，这需要使用者自己解决。
-                    coco-mvc框架采用元数据和装饰器一一对应，运行时生成元数据的方式，
-                    例如首先框架关联<InlineCode>@view</InlineCode>装饰器和<InlineCode>View</InlineCode>元数据类：
+                    装饰器是一种强大的编程特性，ES规范虽然规定了装饰器语法，但框架需要自行解决运行时如何使用装饰器参数的问题。
+                    coco-mvc采用装饰器和元数据一一对应，运行时生成元数据实例的方式，
+                    例如<InlineCode>@view</InlineCode>装饰器对应<InlineCode>View</InlineCode>元数据类：
                 </div>
                 <Code code={this.viewDecorator} />
-                装饰器和元数据类通过<InlineCode>createDecoratorExp</InlineCode>关联。
-                然后业务中给<InlineCode>Button</InlineCode>添加<InlineCode>@view</InlineCode>装饰器，那么运行时<InlineCode>Button</InlineCode>会关联一个<InlineCode>View</InlineCode>实例，这样框架就知道
+                <InlineCode>createDecoratorExp</InlineCode>就是起到对应作用的函数，
+                假设在业务中<InlineCode>Button</InlineCode>添加了<InlineCode>@view</InlineCode>装饰器，运行时框架为<InlineCode>Button</InlineCode>会关联一个<InlineCode>View</InlineCode>实例，这样框架就知道
                 <InlineCode>Button</InlineCode>是一个视图组件。
                 <Code code={this.buttonClass} />
-                <Card>一般来说，元数据类名和装饰器名字应该保持一致（除了首字母大小写不同）。</Card>
-                <Header2>装饰器参数赋值给元数据</Header2>
-                上面说到，框架在运行时会实例化装饰器对应的元数据类，具体来说拿装饰器表达式第一个参数的类型进行分情况讨论：
+                <Card>一般来说，元数据类名和装饰器名字是一致的（除了首字母大小写不同）。</Card>
+                <Header2>组件装饰器</Header2>
+                组件装饰器是指可以将<InlineCode>class</InlineCode>增强为组件的装饰器。<InlineCode>@view</InlineCode>就是组件装饰器，下面列举了框架提供的所有组件装饰器：
                 <ul>
-                    {/* TODO:空对象还有报错：this.props.children.trim is not a function */}
-                    <li>如果类型是纯对象（即<InlineCode>Record&lt;string, any&gt;</InlineCode>
-                    ），则找到该对象自身全部可枚举的field，浅赋值给元数据实例</li>
-                    <li>其他类型：
-                        <ul>
-                            <li>如果元数据类定义了field，那么会获取第一个field，进行赋值</li>
-                            <li>如果元数据类没有定义field，那么默认赋值给"value"这个field</li>
-                        </ul>
-                    </li>
+                    <li>@component</li>
+                    <li>@view</li>
+                    <li>@page</li>
+                    <li>@layout</li>
+                    <li>@flow</li>
+                    <li>@globalData</li>
+                    <li>@render</li>
+                    <li>@api</li>
+                    <li>@util</li>
+                    <li>@localStorage</li>
+                    <li>@sessionStorage</li>
+                    <li>@cookie</li>
+                    <li>@store</li>
+                    <li>@router</li>
                 </ul>
-                
+                <div>
+                    那么有个问题：组件装饰器和其他装饰器（例如：<InlineCode>@bind</InlineCode>）有什么区别？
+                    首先，<InlineCode>@component</InlineCode>是组件装饰器，然后如果一个装饰器对应的元数据类有<InlineCode>@component</InlineCode>装饰器，那么也是组件装饰器，例如<InlineCode>@view</InlineCode>。
+                    然后如果一个装饰器对应的元数据类有<InlineCode>@view</InlineCode>装饰器，那么也是组件装饰器。例如<InlineCode>@page</InlineCode>。
+                </div>
+                <Code code={this.pageDecorator} />
+                <div>
+                    也就是说框架递归地找装饰器元数据类、元数据类的装饰器的元数据类是否包含<InlineCode>@component</InlineCode>装饰器，如果找到的话就是组件装饰器。
+                    框架本身提供的装饰器关系如下（下一级装饰器的元数据类都有上一级的装饰器）：
+                    <Code code={this.componentTreeCode}></Code>
+                    也就是说框架<span className={'text-primary font-bold'}>根据不同业务场景、不同抽象程度提供不同的装饰器</span>：
+                    <ul>
+                        <li><InlineCode>@component</InlineCode>无任何业务含义的组件装饰器</li>
+                        <li>
+                            <ul>
+                                <li>
+                                    <InlineCode>@view</InlineCode>是通用的视图组件装饰器
+                                </li>
+                                <li>
+                                    <InlineCode>@page</InlineCode>是页面组件装饰器
+                                </li>
+                                <li>
+                                    <InlineCode>@layout</InlineCode>是布局组件装饰器
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <ul>
+                                <li>
+                                    <InlineCode>@util</InlineCode>是通用的工具组件装饰器
+                                </li>
+                                <li>
+                                    <InlineCode>@api</InlineCode>都是工具层的组件装饰器
+                                </li>
+                                <li>
+                                    <InlineCode>@localStorage</InlineCode>都是工具层的组件装饰器
+                                </li>
+                                <li>
+                                    <InlineCode>@sessionStorage</InlineCode>都是工具层的组件装饰器
+                                </li>
+                            </ul>
+                        </li>
+                        <li><InlineCode>@flow</InlineCode>表示通用的流程组件装饰器</li>
+                        <li><InlineCode>@store</InlineCode>表示通用的视图层状态组件装饰器</li>
+                        <li><InlineCode>@globalData</InlineCode>表示通用的全局变量组件装饰器</li>
+                    </ul>
+                </div>
+                <Card>
+                    ES装饰器提案处于stage3阶段，语法见：https://github.com/tc39/proposal-decorators。
+                </Card>
             </ContentLayout>
         );
     }
